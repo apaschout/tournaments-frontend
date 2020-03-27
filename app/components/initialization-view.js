516903292b37e30b854ae38a82090837875cc684
Alex Paschout <apaschout@gmail.com>
2020-03-27 16:15:48 +0100

    Add modal

--- app/components/initialization-view.js
+++ app/components/initialization-view.js
@@ -1,13 +1,20 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { getOwner } from '@ember/application';
+import { tracked } from '@glimmer/tracking';
 import jQuery from 'jquery';
 
 export default class InitializationViewComponent extends Component {
     formats = ["cube"]
     gamesToWin = [1, 2, 3]
+    @tracked showingDelModal = false
+
+    @action toggleDelModal() {
+        this.showingDelModal = !this.showingDelModal
+    }
     
     @action postAs(act, id, arg) {
+        let compThis = this
         console.log(arg)
         let data = { "@action": act.rel }
         if (act.rel == "change-name" || act.rel == "change-gamestowin" || act.rel == "change-format") {
@@ -20,7 +27,12 @@ export default class InitializationViewComponent extends Component {
             url: `http://127.0.0.1:8080/api/tournaments/${id}`,
             data: data,
             success: function () {
-                route.refresh()
+                if (act.rel == "delete") {
+                    compThis.toggleDelModal()
+                    route.transitionTo("tournaments")
+                } else {
+                    route.refresh()
+                }
             },
         }).fail(function () { alert(r.responseText) })
     }
